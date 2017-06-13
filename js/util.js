0db24d48de6dc53165982ae05fe5741eaf26590a
saki matsumoto <sa_matsumoto@mit.to>
2017-06-13 21:20:46 +0900

    Initial View

--- js/util.js
+++ js/util.js
@@ -7,9 +7,15 @@
         var d = date.getDate();
         var dStr = d < 10 ? '0' + d : d.toString();
         return h5.u.str.format('{0}-{1}-{2}', y, mStr, dStr);
+    },
+
+    formatTime: function (date) {
+      var h = date.getHours();
+      var m = date.getMinutes();
+      return (h < 10 ? '0' : '') + h + ':' + (m < 10 ? '0' : '') + m;
     }
   });
-  
+
   // ダミー用のajax（サーバを使用しないため、）
   h5.ajax = function() {
      var dfd = h5.async.deferred();
